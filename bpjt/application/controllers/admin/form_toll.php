1c157ebf90323ac7794e6d8703a14b6c90ddd0c8
Muhammad Fauzan Mufti Dhana <fauzanmd5939@gmail.com>
2024-03-05 13:53:33 +0700

    update

--- bpjt/application/controllers/admin/form_toll.php
+++ bpjt/application/controllers/admin/form_toll.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class form_toll extends CI_Controller
+class Form_Toll extends CI_Controller
 {
     public $user_access;
 
